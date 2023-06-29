3c3057fc35039245f17c0628014a5b351f671424
Marcio Martins <marcioamandy@gmail.com>
2023-06-29 16:25:11 -0300

    chore: nova estrutura

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: "coverage",
+    testEnvironment: "node",
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    }
+};
